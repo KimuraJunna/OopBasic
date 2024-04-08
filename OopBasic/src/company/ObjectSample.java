c8ef4b4b490b9a9bf39e6a1173c28a11e25547ac
KimuraJunna <junna.kimura.n134@dirbato.co.jp>
2024-04-08 17:26:31 +0900

    文字列～日付と時刻

--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -1,6 +1,19 @@
 package company;
 
 import java.lang.invoke.VarHandle;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.sql.rowset.spi.SyncResolver;
 
 public class ObjectSample {
 
@@ -21,6 +34,85 @@ public class ObjectSample {
         engineer.report();
         engineer.joinMeeting();
 
-    }
+   //Lesson3 Chapter6.4 [オブジェクト]
+        
+    String a = new String("Hello");
+    String b = new String("Hello");
+    String c = a;
+    
+    System.out.println(a == b);
+    System.out.println(a == c);
+    
+    System.out.println(a.equals(b));
+    
+    Double d = 3.14;
+    System.out.println(d.toString());
+    
+    System.out.println(d.hashCode());
+    
+    
+    //Lesson3 Chapter6.4 [文字列]
+    
+    String str = " Hello World! ";
+    System.out.println(str.length());
+    
+    System.out.println(str.substring(1, 4));
 
+    System.out.println(str.contains("llo"));
+    
+    System.out.println(str.replace('1', 'L'));
+    
+    System.out.println(str.trim());
+    
+    String name = "Alice";
+    int age = 25;
+    double height = 1.7;
+    
+    String formattedString = "Name: %s, Age: %d, Height: %.2f". formatted(name, age, height);
+    System.out.println(formattedString);
+    
+    //Lesson3 Chapter6.4 [数値]
+    
+    System.out.println(Math.abs(-5));
+    
+    System.out.println(Math.ceil(3.3));
+    System.out.println(Math.floor(3.7));
+    System.out.println(Math.round(3.5));
+    
+    System.out.println(Math.max(5,10));
+    System.out.println(Math.min(5,10));
+    
+    System.out.println(Math.random());
+    
+    
+  //Lesson3 Chapter6.4 [日付と時刻]
+    
+    System.out.println(LocalDate.now());
+    System.out.println(LocalDate.of(2023, Month.DECEMBER, 5 ));
+    
+    System.out.println(LocalTime.now());
+    System.out.println(LocalTime.of(13, 45));
+    
+    System.out.println(LocalDateTime.now());
+    System.out.println(LocalDateTime.of(2023, Month.DECEMBER, 5,13,45
+            ));
+    System.out.println(Period.between(LocalDate.of(2023,1,1),LocalDate.of(2023,12,31)));
+    System.out.println(Duration.between(LocalTime.of(9, 1), LocalTime.of(17, 0)));
+    
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    System.out.println(LocalDateTime.now().format(formatter));
+    
+    try {
+        System.out.println(LocalDateTime.parse("2023/12/05 15:30:45", formatter));
+    } catch (DateTimeException e) {
+        System.out.println("Error" + e.getMessage());
+    }
+    
+    var tokyoTime = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+    
+    var utcTime = tokyoTime.withZoneSameInstant(ZoneOffset.UTC);
+    
+    System.out.println("Tokyo Time: " + tokyoTime);
+    System.out.println("UTC Time: " + utcTime);
+    }
 }
